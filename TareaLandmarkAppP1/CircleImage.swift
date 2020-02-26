a4a55f0d89b5ab3ba4eeba6453b8ec862ccc8433
ramon baeza yepez <By_ramon@outlook.com>
2020-02-26 14:52:11 -0600

    Done

---
+++ TareaLandmarkAppP1/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  TareaLandmarkAppP1
+//
+//  Created by ByRamon on 26/02/20.
+//  Copyright © 2020 ByRamon. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.gray, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
